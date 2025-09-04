d84954e29a0f0f1e39d7aee8067d44a01fbfb5c2
LucaniaMC <yangr2002@gmail.com>
2025-09-04 02:49:15 -0500

    Manual skin selection
    Created scripts to allow manually adding skin selection in the customization window.

--- Assets/Scripts/Menu/CustomizeMenu.cs
+++ Assets/Scripts/Menu/CustomizeMenu.cs
@@ -1,6 +1,53 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomizeMenu : UIMenu
 {
+    public PlayerSkinDatabase playerSkinDatabase;
 
+    public Image artworkSprite;
+
+    private int selectedOption = 0;
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("skin")) //Check if there is skin saved
+        {
+            selectedOption = 0;
+        }
+        else
+        {
+            LoadSkin();
+        }
+
+        UpdateSkin(selectedOption);
+
+        //load skin menu
+    }
+
+
+    public void SelectSkin(int option)
+    {
+        selectedOption = option;
+        UpdateSkin(selectedOption);
+    }
+
+
+    private void UpdateSkin(int selectedOption)
+    {
+        PlayerSkin playerSkin = playerSkinDatabase.GetSkin(selectedOption);
+        //artworkSprite.sprite = playerSkin.playerSprite;
+    }
+    
+    
+    public void LoadSkin()
+    {
+        selectedOption = PlayerPrefs.GetInt("skin");
+    }
+
+
+    public void SaveSkin()
+    {
+        PlayerPrefs.SetInt("skin", selectedOption);
+    }
 }
