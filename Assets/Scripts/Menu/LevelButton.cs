e63bddab890953f84c9b702091a52efeb60b0043
LucaniaMC <yangr2002@gmail.com>
2025-09-10 00:05:33 -0500

    Level button script
    Created a script for level button.

---
+++ Assets/Scripts/Menu/LevelButton.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class LevelButton : MonoBehaviour
+{
+    public string LevelNumber;
+    public int starRating = 0;
+    [SerializeField] private GameObject[] stars;
+}
