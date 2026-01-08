ee2987e1912938baa7088113e8676055ebe33c89
LucaniaMC <yangr2002@gmail.com>
2026-01-07 22:59:22 -0600

    Pointer Rotation
    Added sprite to the pointer.
    
    Pointer now rotates towards the player.

--- Assets/Scripts/Player/ScreenPointer.cs
+++ Assets/Scripts/Player/ScreenPointer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ScreenPointer : MonoBehaviour
 {
     public GameObject pointer; //Prefab of the pointer sprite
+    GameObject pointerInstance;
     Camera mainCamera;
     GameObject player;
 
@@ -12,11 +13,13 @@ public class ScreenPointer : MonoBehaviour
     {
         mainCamera = Camera.main;
         player = LevelManager.instance.player;
+        pointerInstance = Instantiate(pointer);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float angle = Mathf.Atan2(player.transform.position.y, player.transform.position.x) * Mathf.Rad2Deg;
+        pointerInstance.transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 }
