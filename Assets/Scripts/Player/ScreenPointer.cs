84f139567fa131324ca7894fd2d0feeb1af36e18
LucaniaMC <yangr2002@gmail.com>
2026-01-07 23:08:25 -0600

    Pointer enable and disable
    The pointer is now enabled if the target object is off screen, and disabled if the object is on screen.

--- Assets/Scripts/Player/ScreenPointer.cs
+++ Assets/Scripts/Player/ScreenPointer.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Attach this script to an object to create a pointer towards it from the player when the object is off screen
 public class ScreenPointer : MonoBehaviour
 {
-    public GameObject pointer; //Prefab of the pointer sprite
-    GameObject pointerInstance;
+    public GameObject pointer; //Prefab of the pointer
+    GameObject pointerInstance; //Active instance of the pointer
     Camera mainCamera;
     GameObject player;
 
@@ -22,5 +23,18 @@ public class ScreenPointer : MonoBehaviour
         //Rotates the pointer away from player
         float angle = Mathf.Atan2(player.transform.position.y, player.transform.position.x) * Mathf.Rad2Deg;
         pointerInstance.transform.rotation = Quaternion.Euler(0f, 0f, angle - 180f);
+
+        //check if the object is off screen
+        var screenPos = mainCamera.WorldToViewportPoint(transform.position);
+        bool isOffScreen = screenPos.x < 0 || screenPos.x > 1 || screenPos.y < 0 || screenPos.y > 1;
+
+        if (isOffScreen)
+        {
+            pointerInstance.SetActive(true);
+        }
+        else
+        {
+            pointerInstance.SetActive(false);
+        }
     }
 }
