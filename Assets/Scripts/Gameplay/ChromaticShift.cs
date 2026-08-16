8e8b93b5f71a9c9fb82064597caf9c78b065a43f
LucaniaMC <yangr2002@gmail.com>
2026-08-16 10:55:58 -0500

    Chromatic Shift Idle Effect
    Created a script to add a subtle idle chromatic shift effect that fades in and out in intervals.
    
    Added goal and refuel points to snow test level.

---
+++ Assets/Scripts/Gameplay/ChromaticShift.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+//Controls the intensity of the chromatic shift effect for space level
+//Includng an idle looping effect, and increase intensity when getting near black holes (TBA)
+public class ChromaticShift : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+
+    //Subtle idle effect that fades in and out
+    float idleShiftAmount = 0f; //Shift amount input into the material
+    float idleSpeedModifier = 2f;   //Speed for the idle loop
+    float idleMaxIntensity = 0.15f;  //Maximum shift amount for the idle loop
+
+
+    void Update()
+    {
+        //Idle Effect
+        float sin = Mathf.Sin(Time.time * idleSpeedModifier);
+        float clampedSin = Mathf.Clamp(sin, -0.5f, 0.5f);
+        float shiftedSin = clampedSin + 0.5f;
+        idleShiftAmount = shiftedSin * idleMaxIntensity;
+
+        sprite.material.SetFloat("_ShiftAmount", idleShiftAmount);
+    }
+}
